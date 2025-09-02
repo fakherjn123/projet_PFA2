6c9a884f4e74733c429c01de8a6c53e0a49f5d62
fakherjn123 <fakher.jn2004@gmail.com>
2025-09-02 04:10:57 +0200

    Create osController.js

---
+++ controllers/osController.js
@@ -0,0 +1,12 @@
+const osService = require('../service/osService')
+
+module.exports.getOsInformation = async (req,res)=>{
+    try {   
+        const osInformation = await osService.getData()
+                console.log(osInformation)
+
+        res.status(200).json({osInformation})
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
