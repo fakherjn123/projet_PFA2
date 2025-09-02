3765e47891924d2782b92abf5454ecbcae92ca27
fakherjn123 <fakher.jn2004@gmail.com>
2025-09-02 05:04:20 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose")
 
 module.exports.connecttoMongoDB = async () => {
-  mongoose.set('strictQuery', false);
-  mongoose.connect(process.env.Url_Mongo).then(()=>{console.log("connect to db")}
-).catch(
-    (error)=>{console.log(error)}
-  )
+    mongoose.set('strictQuery', false);
+    mongoose.connect(process.env.Url_Mongo).then(() => { console.log("connect to db") }
+    ).catch(
+        (error) => { console.log(error) }
+    )
 }
 
 //projet_PFA2
