9b0ef25de01f63f1d9b15ddf4a511e682a4e2838
fakherjn123 <fakher.jn2004@gmail.com>
2025-10-02 01:43:20 +0200

    Update osRouter.js

--- routes/osRouter.js
+++ routes/osRouter.js
@@ -1,22 +1,9 @@
 var express = require('express');
 var router = express.Router();
-// const os = require('os')
-const osController = require('../controllers/osController')
-/* GET home page. */
-router.get('/getOsInformation',osController.getOsInformation )
-// router.get('/getOsInformation', function(req, res, next) {
-//     try {
-//         const osInformation = {
-//             hostname : os.hostname(),
-//             platform : os.platform(),
-//             type : os.type(),
-//             release : os.release()
-//         }
-//         console.log(osInformation)
-//         res.status(200).json(osInformation)
-//     } catch (error) {
-//         res.status(500).json({message: error.message})
-//     }
-// });
+const osController = require('../controllers/osController');
+
+// Existing route
+router.get('/getOsInformation', osController.getOsInformation);
+
 
 module.exports = router;
